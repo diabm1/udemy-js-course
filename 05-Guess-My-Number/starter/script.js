48ec0257970a1d2c60d51fce7d6212ab15de894b
diabm1 <diabm1855@gmail.com>
2022-10-21 18:32:12 -0400

    first commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,81 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // console.log(guess, typeof guess)
+
+  // when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+  // if player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // score++;
+    // document.querySelector('.score').textContent = score;
+  }
+  // when guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // when guess is too low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Coding Challenge #1
+
+
+// Implement a game rest functionality, so that player can make a new huess! Here's how:
+
+// 1. Select the element with the 'again' class and attach a click event handler
+
+document.querySelector('.again').addEventListener('click', function (){
+  // 2. In the handler function, restore initial values of the score and number variables
+  score = 20
+  // not sure what the number variables are
+
+  // 3.
+
+})
+
+
+
