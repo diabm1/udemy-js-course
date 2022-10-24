51e9ddb026adee0c874eff0798ee6922c25edacc
diabm1 <diabm1855@gmail.com>
2022-10-24 14:45:32 -0400

    making some more changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,10 +12,8 @@
 
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -63,19 +61,24 @@ document.querySelector('.check').addEventListener('click', function () {
 
 // Coding Challenge #1
 
-
 // Implement a game rest functionality, so that player can make a new huess! Here's how:
 
 // 1. Select the element with the 'again' class and attach a click event handler
 
-document.querySelector('.again').addEventListener('click', function (){
+document.querySelector('.again').addEventListener('click', function () {
   // 2. In the handler function, restore initial values of the score and number variables
-  score = 20
+  score = 20;
   // not sure what the number variables are
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  // 3.
+  // 3. Restore the initial conditions of the message, number, score, and guess input field
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  // idk how to restore the initial conditions of guess input field
 
-})
-
-
-
+  //4. Also restore the original background color (#222) and number width (15rem)
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
