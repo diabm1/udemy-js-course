682ee0537754177da95b1c2c774170a7ea79ded0
diabm1 <diabm1855@gmail.com>
2022-10-31 16:33:04 -0400

    working through the course

---
+++ 01-Fundamentals-Part-1/starter/asset/js/script.js
@@ -0,0 +1,52 @@
+// let js = "amazing";
+// console.log(48 + 28 + 12 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Jonas";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+const country = "USA";
+const continent = "North America";
+let population = 331900000;
+let populationDivided = population / 2;
+
+console.log(populationDivided / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population > 3);
+
+let language = "english";
+let description =
+  country +
+  " is in " +
+  continent +
+  " , and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = "USA";
+isIsland = true;
+
+
+
+// console.log(typeof isIsland)
+// console.log(typeof population)
+// console.log(typeof country)
+// console.log(typeof language)
+
+
+////////////////////////////////
+//Coding Challenge #1
+
+
