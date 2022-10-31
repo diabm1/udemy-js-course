beb5b95d8948e729801376183ef23cb723bd3493
diabm1 <diabm1855@gmail.com>
2022-10-31 17:20:38 -0400

    went through first challenge problem

--- 01-Fundamentals-Part-1/starter/asset/js/script.js
+++ 01-Fundamentals-Part-1/starter/asset/js/script.js
@@ -9,44 +9,71 @@
 // console.log(firstName);
 // console.log(firstName);
 
-const country = "USA";
-const continent = "North America";
-let population = 331900000;
-let populationDivided = population / 2;
+// const country = "USA";
+// const continent = "North America";
+// let population = 331900000;
+// let populationDivided = population / 2;
 
-console.log(populationDivided / 2);
-population++;
-console.log(population);
-console.log(population > 6);
-console.log(population > 3);
+// console.log(populationDivided / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population > 3);
 
-let language = "english";
-let description =
-  country +
-  " is in " +
-  continent +
-  " , and its " +
-  population +
-  " million people speak " +
-  language;
-console.log(description);
+// let language = "english";
+// let description =
+//   country +
+//   " is in " +
+//   continent +
+//   " , and its " +
+//   population +
+//   " million people speak " +
+//   language;
+// console.log(description);
 
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 
-let isIsland = "USA";
-isIsland = true;
-
-
+// let isIsland = "USA";
+// isIsland = true;
 
 // console.log(typeof isIsland)
 // console.log(typeof population)
 // console.log(typeof country)
 // console.log(typeof language)
 
-
 ////////////////////////////////
 //Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass/height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall
+*/
+
+//Marks
+// const markMass = 78;
+// const markHeight = 1.69;
+// const markMass = 95;
+// const markHeight = 1.88;
+
+//John
+// const johnMass = 92;
+// const johnHeight = 1.95;
+// const johnMass = 85;
+// const johnHeight = 1.76;
+
+// const markBMI = markMass / markHeight ** 2;
+// const johnBMI = johnMass / (johnHeight * johnHeight);
+// const markHigherBMI = markBMI > johnBMI;
+
+// console.log(markBMI, johnBMI, markHigherBMI);
+
 
 
